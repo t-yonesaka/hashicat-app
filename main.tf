928b83393c9f0d9b475ff1e75c7bdf90d5716aad
t-yonesaka <takeshi.yonesaka@ibm.com>
2025-05-14 06:36:44 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ resource "aws_key_pair" "hashicat" {
 }
 
 module "s3-bucket" {
-  source              = "cloudposse/s3-bucket/aws"
+  source              = "app.terraform.io/tyonesaka-org/s3-bucket/aws"
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
